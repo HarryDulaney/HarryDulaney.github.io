91a1b4f8969776d9ec456756bad4b0c469aa0447
Harry G. Dulaney  IV <harrydulaney@gmail.com>
2020-07-28 11:08:09 -0400

    Added project card, improved nav bar event bindings by adding
    single click close on icon.

--- assets/js/functions2.js
+++ assets/js/functions2.js
@@ -1,12 +1,19 @@
 //@author Harry Dulaney
 let open = 0;
 $(document).ready(function () {
+
     $('#expandMenu').on('click', function () {
         if (open === 0) {
             $('#mobile--navigation').css('display', 'block');
             $('#navbar').css('height', '200px');
             open = 1;
+        } else if (open === 1) {
+            $('#mobile--navigation').css('display','none');
+            $('#navbar').css('height','fit-content');
+            open = 0;
+
         }
+
     })
 
     $('#navbar').on('dblclick', function () {
