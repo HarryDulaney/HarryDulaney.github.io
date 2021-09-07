4b507d38696478f49bedfe25398ee22c07aff3c0
Harry G. Dulaney  IV <harrydulaney@gmail.com>
2021-09-06 20:59:43 -0400

    animate mobile navigation button

--- assets/js/harrydulaneydotcom.js
+++ assets/js/harrydulaneydotcom.js
@@ -1,22 +1,17 @@
 /** @author Harry Dulaney */
 
-var open, mainNavVisible, max1180, max900, max767, max600, max360, mtl;
+var open = false;
+var mainNavVisible = true;
+var max1180 = window.matchMedia("(max-width: 1180px)");
+var max900 = window.matchMedia("(max-width: 900px)");
+var max767 = window.matchMedia("(max-width: 767)");
+var max600 = window.matchMedia("(max-width: 600px)");
+var max360 = window.matchMedia("(max-width: 360px)");
 
 $(document).ready(function () {
-    open = false;
-    mainNavVisible = true;
-    max1180 = window.matchMedia("(max-width: 1180px)");
-    max900 = window.matchMedia("(max-width: 900px)");
-    max767 = window.matchMedia("(max-width: 767)");
-    max600 = window.matchMedia("(max-width: 600px)");
-    max360 = window.matchMedia("(max-width: 360px)");
-    mtl = gsap.timeline({defaults: {duration: 1, ease: 'expo.inOut'}});
-
     $(window).on('scroll', function () {
         if (open) {
             retractMobileMenu();
-        }
-        if (max1180.matches || max900.matches) {
         }
     });
 
@@ -37,18 +32,18 @@ $(document).ready(function () {
 });
 
 function retractMobileMenu() {
-    mtl.to('.close', {opacity: 0, duration: 0.2}, '-=0.5')
-        .to('.top--navbar', {height: '41px', duration: 0.2}, '-=0.5')
-        .to('.mobile--nav', {display: 'none', duration: 0.2}, '-=0.5')
-        .to('.bars', {opacity: 1, duration: 0.1}, '-=0.5')
-        .to('.top--navbar', {height: 'fit-content',duration:0.1}, '-=0.5');
+    gsap.to('.close', {opacity: 0, duration: 0.2});
+    gsap.to('.top--navbar', {height: '40px', duration: 0.2});
+    gsap.to('.mobile--nav', {display: 'none', duration: 0.2});
+    gsap.to('.bars', {opacity: 1, duration: 0.1});
+    gsap.to('.top--navbar', {height: 'fit-content', duration: 0.1});
     open = false;
 }
 
 function expandMobileMenu() {
-    mtl.to('.top--navbar', {height: '185px', duration: 0.1}, '-=0.5')
-        .to('.bars', {opacity: 0, duration: 0.1}, '-=0.5')
-        .to('.mobile--nav', {display: 'block', duration: 0.2}, '-=0.5')
-        .to('.close', {opacity: 1, duration: 0.2}, '-=0.5');
+    gsap.to('.top--navbar', {height: '185px', duration: 0.1})
+    gsap.to('.bars', {opacity: 0, duration: 0.1});
+    gsap.to('.mobile--nav', {display: 'block', duration: 0.2})
+    gsap.to('.close', {opacity: 1, duration: 0.2});
     open = true;
 }
