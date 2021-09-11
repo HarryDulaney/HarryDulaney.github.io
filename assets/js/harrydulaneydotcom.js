55d30af4150857cd2d07136f61fce9e9b4a05cf3
Harry G. Dulaney  IV <harrydulaney@gmail.com>
2021-09-10 21:42:55 -0400

    animate nav bar

--- assets/js/harrydulaneydotcom.js
+++ assets/js/harrydulaneydotcom.js
@@ -7,27 +7,53 @@ var max900 = window.matchMedia("(max-width: 900px)");
 var max767 = window.matchMedia("(max-width: 767)");
 var max600 = window.matchMedia("(max-width: 600px)");
 var max360 = window.matchMedia("(max-width: 360px)");
+var min900 = window.matchMedia("(min-width: 900)");
+var min600 = window.matchMedia("(min-width: 600)");
 
 $(document).ready(function () {
+    var initialScrollPos = window.pageYOffset;
+
     $(window).on('scroll', function () {
-        if (open) {
-            retractMobileMenu();
+        //Mobile scroll behavior
+        if (max600.matches) {
+            if (open) {
+                retractMobileMenu();
+            }
+        } else {
+            // Regular Nav Bar behavior
+            var currentScrollPos = window.pageYOffset;
+            if (initialScrollPos > currentScrollPos) {
+                    expandNavbar();
+            } else {
+                hideNavBar();
+            }
+            initialScrollPos = currentScrollPos;
         }
     });
 
     $('.mobile-nav-toggle').on('click', function () {
-        if (!open) {
-            expandMobileMenu();
-        } else if (open) {
-            retractMobileMenu();
+        //Mobile nav click behavior
+        if (max600.matches) {
+            if (!open) {
+                expandMobileMenu();
+            } else if (open) {
+                retractMobileMenu();
+            }
         }
+
 
     });
 
     $('.main--body').on('click', function () {
-        if (open) {
-            retractMobileMenu();
+        //Mobile body click behavior
+        if (max600.matches) {
+            if (open) {
+                retractMobileMenu();
+            }
+        } else {
+            // Regular Nav Behavior
         }
+
     });
 });
 
@@ -40,10 +66,20 @@ function retractMobileMenu() {
     open = false;
 }
 
+function expandNavbar() {
+    $('.top--navbar').css('top','0px');
+}
+
+function hideNavBar() {
+    $('.top--navbar').css('top','-50px');
+}
+
+
 function expandMobileMenu() {
     gsap.to('.top--navbar', {height: '185px', duration: 0.1})
     gsap.to('.bars', {opacity: 0, duration: 0.1});
     gsap.to('.mobile--nav', {display: 'block', duration: 0.2})
     gsap.to('.close', {opacity: 1, duration: 0.2});
     open = true;
+
 }
