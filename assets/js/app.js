d0657e00abf0d058d7f369c32867cdb1f5f402c3
Harry Dulaney <harrydulaney@gmail.com>
2023-06-28 04:13:43 -0400

    style updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -324,7 +324,7 @@ function disableSubmitButton() {
     $('#submit-button-contact-form').prop('disabled', true);
 }
 
-function initContactForm() {
+function initializeContactForm() {
     var form = document.getElementById('contact-form');
     form.addEventListener("submit", submitContactForm);
 }
