e3828058a81c232e9be88ab450a7f40f36512568
Harry Dulaney <harrydulaney@gmail.com>
2023-08-07 05:01:58 -0400

    fix download recap

--- assets/js/download.js
+++ assets/js/download.js
@@ -2,6 +2,7 @@
 //download.js
 let downloadPageRecaptchaValidated = false;
 /* --------------- Handle download page recapatcha--------------- */
+
 /**
  * Enable the resume download button
  * @param {*} token 
@@ -10,11 +11,11 @@ function enableResumeDownloadButton(token) {
     downloadPageRecaptchaValidated = true;
     $('#resume--download-icon').removeClass('fas fa-window-close');
     $('#resume--download-icon').addClass('fas fa-download');
-
-    $('#resume--download-link').prop('disabled', false);
-    $('#resume--download-link').prop('href', "assets/img/HarryDulaneyResume.pdf");
-    $('#resume--download-link').prop('download', "HarryDulaneyResume");
-
+    $('.download--wrapper .download--image').prop('disabled', false);
+    $('.download--wrapper .download--image').prop('href', "assets/img/HarryDulaneyResume.pdf");
+    $('.download--wrapper .download--image').prop('download', "HarryDulaneyResume");
+    $(".download--wrapper .download--image").css('cursor', 'pointer');
+    $('.download--wrapper .download--image').attr('title', 'Download my resume.');
 }
 
 /**
@@ -24,8 +25,9 @@ function disableResumeDownloadButton() {
     downloadPageRecaptchaValidated = false;
     $('#resume--download-icon').removeClass('fas fa-download');
     $('#resume--download-icon').addClass('fas fa-window-close');
-
-    $('#resume--download-link').prop('disabled', true);
-    $('#resume--download-link').prop('href', "#");
-    $('#resume--download-link').prop('download', undefined);
+    $('.download--wrapper .download--image').prop('href', "#");
+    $('.download--wrapper .download--image').prop('download', undefined);
+    $('.download--wrapper.download--image').css('cursor', 'not-allowed');
+    $('.download--wrapper.download--image').attr('title', 'Please complete the recaptcha to download my resume.');
+    $('.download--wrapper .download--image').prop('disabled', true);
 }
