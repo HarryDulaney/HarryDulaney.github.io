cad27b0101240d47a0d8182510c7e3d53b45b7a1
Harry Dulaney <harrydulaney@gmail.com>
2023-08-07 06:21:18 -0400

    refactor + fix download recaptcha

--- assets/js/download.js
+++ assets/js/download.js
@@ -2,6 +2,27 @@
 //download.js
 let downloadPageRecaptchaValidated = false;
 /* --------------- Handle download page recapatcha--------------- */
+/**
+ * Onload callback for the Download page recaptcha
+ */
+function onloadDownloadRecaptcha() {
+    if(downloadPageRecaptchaValidated) {
+        $('#resume--download-icon').removeClass('fas fa-window-close');
+        $('#resume--download-icon').addClass('fas fa-download');
+        $('.download--wrapper .downloads .download--image').prop('disabled', false);
+        $('.download--wrapper .downloads .download--image').prop('href', "assets/img/HarryDulaneyResume.pdf");
+        $('.download--wrapper .downloads .download--image').prop('download', "HarryDulaneyResume");
+        $('.download--wrapper .downloads .download--image').attr('title', 'Download Ready!.');
+    } else {
+        $('#resume--download-icon').removeClass('fas fa-download');
+        $('#resume--download-icon').addClass('fas fa-window-close');
+        $('.download--wrapper .downloads .download--image').prop('href', "#");
+        $('.download--wrapper .downloads .download--image').prop('download', undefined);
+        $('.download--wrapper .downloads .download--image').attr('title', 'Please complete the recaptcha to download my resume.');
+        $('.download--wrapper .downloads .download--image').prop('disabled', true);
+    }
+
+}
 
 /**
  * Enable the resume download button
@@ -11,11 +32,10 @@ function enableResumeDownloadButton(token) {
     downloadPageRecaptchaValidated = true;
     $('#resume--download-icon').removeClass('fas fa-window-close');
     $('#resume--download-icon').addClass('fas fa-download');
-    $('.download--wrapper .download--image').prop('disabled', false);
-    $('.download--wrapper .download--image').prop('href', "assets/img/HarryDulaneyResume.pdf");
-    $('.download--wrapper .download--image').prop('download', "HarryDulaneyResume");
-    $(".download--wrapper .download--image").css('cursor', 'pointer');
-    $('.download--wrapper .download--image').attr('title', 'Download my resume.');
+    $('.download--wrapper .downloads .download--image').prop('disabled', false);
+    $('.download--wrapper .downloads .download--image').prop('href', "assets/img/HarryDulaneyResume.pdf");
+    $('.download--wrapper .downloads .download--image').prop('download', "HarryDulaneyResume");
+    $('.download--wrapper .downloads .download--image').attr('title', 'Download Ready!.');
 }
 
 /**
@@ -25,9 +45,8 @@ function disableResumeDownloadButton() {
     downloadPageRecaptchaValidated = false;
     $('#resume--download-icon').removeClass('fas fa-download');
     $('#resume--download-icon').addClass('fas fa-window-close');
-    $('.download--wrapper .download--image').prop('href', "#");
-    $('.download--wrapper .download--image').prop('download', undefined);
-    $('.download--wrapper.download--image').css('cursor', 'not-allowed');
-    $('.download--wrapper.download--image').attr('title', 'Please complete the recaptcha to download my resume.');
-    $('.download--wrapper .download--image').prop('disabled', true);
+    $('.download--wrapper .downloads .download--image').prop('href', "#");
+    $('.download--wrapper .downloads .download--image').prop('download', null);
+    $('.download--wrapper .downloads .download--image').prop('title', 'Please complete the recaptcha to download my resume.');
+    $('.download--wrapper .downloads .download--image').prop('disabled', true);
 }
